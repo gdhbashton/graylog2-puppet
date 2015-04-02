6291e95a99702ea5c4139d2fbdb7b17e26d3c6e6
Gavin Hamill <gavin@bashton.com>
2015-04-02 10:18:08 +0100

    Do not run the service - oneiota branch only

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -14,26 +14,4 @@ class graylog2::server::service (
   $service_enable,
 ) {
 
-  $service_provider = $::operatingsystem ? {
-    'Ubuntu' => 'upstart',
-    default  => undef,
-  }
-
-  if $service_provider {
-    service { $service_name:
-      ensure     => $service_ensure,
-      enable     => $service_enable,
-      hasstatus  => true,
-      hasrestart => true,
-      provider   => $service_provider,
-    }
-  } else {
-    service { $service_name:
-      ensure     => $service_ensure,
-      enable     => $service_enable,
-      hasstatus  => true,
-      hasrestart => true,
-    }
-  }
-
 }
